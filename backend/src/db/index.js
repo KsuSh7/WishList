f9794865ee37c7ea164760dd8af5fe0a34635e17
KsuSh7 <shevchuk.kseniia1@student.uzhnu.edu.ua>
2026-02-01 15:36:40 +0000

    fixed index

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,7 +1,7 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-export const db = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
